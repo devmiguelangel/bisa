da05fb7bc873e68fb41bb50b95cff381a3b4cdae
Miguel MGM <djmiguelarango@gmail.com>
2015-08-26 16:03:32 -0400

    Reps. Filtro Fecha

--- RP-policy.inc.php
+++ RP-policy.inc.php
@@ -237,10 +237,10 @@ if($rsEx->data_seek(0) === TRUE){
 
             <label style="width: au">Fecha: </label>
             <label style="width:auto;">desde: </label>
-            <input type="text" id="frp-date-b" name="frp-date-b" value="" autocomplete="off" class="date" readonly>
+            <input type="text" id="frp-date-b-<?= strtolower($rowMenu['producto']) ;?>" name="frp-date-b" value="" autocomplete="off" class="date" readonly>
             
             <label style="width:auto;">hasta: </label>
-            <input type="text" id="frp-date-e" name="frp-date-e" value="" autocomplete="off" class="date" readonly><br>
+            <input type="text" id="frp-date-e-<?= strtolower($rowMenu['producto']) ;?>" name="frp-date-e" value="" autocomplete="off" class="date" readonly><br>
             
             <input type="hidden" id="frp-id-user" name="frp-id-user" value="<?=$_SESSION['idUser'];?>">
             <input type="hidden" id="ms" name="ms" value="<?=$_GET['ms'];?>">
